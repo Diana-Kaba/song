43c69b6adb36443d46eed2e029fd7fc22a0fad92
Diana <kabadiana327@gmail.com>
2022-09-11 12:23:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+for (let count = 99; count > 0; count--) {
+  document.write(
+    count +
+      " bottles of beer on the wall,<br>" +
+      count +
+      " bottles of beer,<br> If one of those bottles should happen to fall<br>"
+  );
+}
+
+document.write(
+  "No more bottles of beer on the wall, no more bottles of beer.<br>There's nothing else to fall, because there's no more bottles of beer on the wall."
+);
