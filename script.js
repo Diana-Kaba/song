c937b478925d2286e1d5bf4b628f239a1fd98472
Diana <kabadiana327@gmail.com>
2022-09-16 17:00:23 +0300

    attempt to do on 12

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 "use strict";
-for (let count = 99; count > 0; count--) {
+let drink = prompt("Введите название напитка:");
+for (let count = prompt("Введите количество напитка:"); count > 0; count--) {
   document.write(
     count +
-      " bottles of beer on the wall,<br>" +
+      " bottles of" +
+      " " +
+      drink +
+      " " +
+      "on the wall,<br>" +
       count +
-      " bottles of beer,<br> If one of those bottles should happen to fall<br>"
+      " bottles of" +
+      " " +
+      drink +
+      " " +
+      ",<br> If one of those bottles should happen to fall<br>"
   );
 }
 
 document.write(
-  "No more bottles of beer on the wall, no more bottles of beer.<br>There's nothing else to fall, because there's no more bottles of beer on the wall."
+  "No more bottles of" +
+    " " +
+    drink +
+    " " +
+    "on the wall, no more bottles of" +
+    " " +
+    drink +
+    ".<br>There's nothing else to fall, because there's no more bottles of" +
+    " " +
+    drink +
+    " " +
+    "on the wall."
 );
